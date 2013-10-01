50679cc7ca8baaa925fb5c8807532f4b5b688f91
xmulyj <xmulyj@gmail.com>
2013-10-01 12:52:45 +0800

    key define

--- src/KeyDefine.h
+++ src/KeyDefine.h
@@ -8,36 +8,34 @@
 #ifndef KEYDEFINE_H_
 #define KEYDEFINE_H_
 
-//////////////////////////
-//    Protocol Value    //
-//////////////////////////
-#define ReportRoomInfo  0
-#define GetAllRoom      1
-#define GetAllRoomRsp   2
-#define GetRoomAddr     3
-#define GetRoomAddrRsp  4
-#define IntoRoom        5
-#define IntoRoomRsp     6
-#define OutRoom         7
-#define GetRoomInfo     8
-#define IntoTable       9
-#define OutTable        10
+//Protocol Type
+//////////////////////////////
+#define ReportRoomInfo      0
+#define GetAllRoom          1
+#define GetAllRoomRsp       2
+#define GetRoomAddr         3
+#define GetRoomAddrRsp      4
+#define IntoRoom            5
+#define IntoRoomRsp         6
+#define OutRoom             7
+#define GetRoomInfo         8
+#define IntoTable           9
+#define OutTable            10
 
-///////////////////////////
-//    Key Name Define    //
-///////////////////////////
-#define KEY_Protocol    0
-#define KEY_RoomID
-#define KEY_RoomIP
-#define KEY_RoomPort
-#define KEY_ClientNum
-#define KEY_TableNum
-#define KEY_NumArray
-#define KEY_ClientID
-#define KEY_ClientName
-#define KEY_RoomNum
-#define KEY_WelcomeMsg
-#define KEY_Status
+//KeyName Define
+//////////////////////////////
+#define KEY_Protocol        0    //value:见Protocol Type
+#define KEY_RoomID          1
+#define KEY_RoomIP          2
+#define KEY_RoomPort        3
+#define KEY_ClientNum       4
+#define KEY_TableNum        5
+#define KEY_NumArray        6
+#define KEY_ClientID        7
+#define KEY_ClientName      8
+#define KEY_RoomNum         9
+#define KEY_WelcomeMsg      10
+#define KEY_Status          11
 
 
 #endif /* KEYDEFINE_H_ */
