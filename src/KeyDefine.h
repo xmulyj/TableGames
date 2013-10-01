72626ccaf323c69939d5c32ca4939ad0add21e0c
xmulyj <xmulyj@gmail.com>
2013-10-01 08:27:20 +0800

    doc

---
+++ src/KeyDefine.h
@@ -0,0 +1,43 @@
+/*
+ * KeyDefine.h
+ *
+ *  Created on: Oct 1, 2013
+ *      Author: tim
+ */
+
+#ifndef KEYDEFINE_H_
+#define KEYDEFINE_H_
+
+//////////////////////////
+//    Protocol Value    //
+//////////////////////////
+#define ReportRoomInfo  0
+#define GetAllRoom      1
+#define GetAllRoomRsp   2
+#define GetRoomAddr     3
+#define GetRoomAddrRsp  4
+#define IntoRoom        5
+#define IntoRoomRsp     6
+#define OutRoom         7
+#define GetRoomInfo     8
+#define IntoTable       9
+#define OutTable        10
+
+///////////////////////////
+//    Key Name Define    //
+///////////////////////////
+#define KEY_Protocol    0
+#define KEY_RoomID
+#define KEY_RoomIP
+#define KEY_RoomPort
+#define KEY_ClientNum
+#define KEY_TableNum
+#define KEY_NumArray
+#define KEY_ClientID
+#define KEY_ClientName
+#define KEY_RoomNum
+#define KEY_WelcomeMsg
+#define KEY_Status
+
+
+#endif /* KEYDEFINE_H_ */
